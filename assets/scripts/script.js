6000f826896379ccbcab02c9d65476a16b3cfff0
Kwet <encormoi23@gmail.com>
2023-09-17 11:29:19 +0200

    js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,51 @@
-window.addEventListener('scroll', function () {
-    if (window.scrollY > 350) {
-        document.querySelector('nav').style.position = "fixed";
-        document.querySelector('nav').style.top = "0";
-        document.querySelector('nav').style.margin = "auto";
-
-    } else {
-        document.querySelector('nav').style.position = "relative";
-        document.querySelector('nav').style.top = "auto";
-        document.querySelector('nav').style.margin = "1rem auto";
-    }
-})
-
 let burger = document.querySelector('.burger_icon');
+let btest = false;
 if (burger) {
-    burger.addEventListener('click', function () {
-        let list = document.querySelector('.burger');
-        list.style.display = "block";
 
+    burger.addEventListener('click', function () {
+        if (btest == false) {
+            let list = document.querySelector('.burger');
+            list.style.display = "flex";
+            list.style.flexDirection = "column";
+            document.querySelector('nav').style.flexDirection = "column";
+            btest = true;
+
+        } else {
+            let list = document.querySelector('.burger');
+            list.style.display = "none";
+            list.style.flexDirection = "row";
+            document.querySelector('nav').style.flexDirection = "row";
+            btest = false;
+        }
     })
+
+    window.onresize = function () {
+        if (window.innerWidth < 750) {
+            window.addEventListener('scroll', function () {
+
+                if (window.scrollY > 950) {
+                    document.querySelector('nav').style.position = "fixed";
+                    document.querySelector('nav').style.top = "0";
+                    document.querySelector('nav').style.margin = "1rem auto";
+                } else {
+                    document.querySelector('nav').style.position = "relative";
+                    document.querySelector('nav').style.top = "0";
+                    document.querySelector('nav').style.margin = "1rem auto";
+                }
+            })
+        } else {
+            window.addEventListener('scroll', function () {
+
+                if (window.scrollY > 150) {
+                    document.querySelector('nav').style.position = "fixed";
+                    document.querySelector('nav').style.top = "0";
+                    document.querySelector('nav').style.margin = "1rem auto";
+                } else {
+                    document.querySelector('nav').style.position = "relative";
+                    document.querySelector('nav').style.top = "0";
+                    document.querySelector('nav').style.margin = "1rem auto";
+                }
+            })
+        }
+    }
 }
